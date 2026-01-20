2649967fed8b01d3c0d003f6c2d7daad9b4076d6
Michaela D <deftereos_mi@student.kings.edu.au>
2026-01-20 16:29:15 +1100

    restore original route to index.html (login page)

--- app.py
+++ app.py
@@ -5,22 +5,5 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-@app.route('/dashboard')
-def dashboard():
-	return render_template('dashboard.html')
-
-@app.route('/create_task', methods=["GET", "POST"])
-def create_task():
-    return render_template('create_task.html')
-
-@app.route('/delete_task')
-def delete_task():
-	return render_template('delete_task.html')
-
-@app.route('/update_task')
-def update_task():
-	return render_template('update_task.html')
-
-
 if __name__ == '__main__':
 	app.run()
