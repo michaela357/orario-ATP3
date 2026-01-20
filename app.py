a1bc08f909b39080a783c1d19ef2eeead6f8ba51
Michaela D <deftereos_mi@student.kings.edu.au>
2026-01-20 14:53:55 +1100

    Create routes to login and register an account

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, request, g
 
 app = Flask(__name__)
 
-@app.route("/home")
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run()
