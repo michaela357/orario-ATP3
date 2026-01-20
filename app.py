79b2cf6d71342668082aa56c2200c38928568286
Michaela D <deftereos_mi@student.kings.edu.au>
2026-01-20 16:25:20 +1100

    Create initial routes for each action

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, redirect, request
 app = Flask(__name__)
 
-@app.route("/home")
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+	return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
 
+@app.route('/create_task', methods=["GET", "POST"])
+def create_task():
+    return render_template('create_task.html')
+
+@app.route('/delete_task')
+def delete_task():
+	return render_template('delete_task.html')
+
+@app.route('/update_task')
+def update_task():
+	return render_template('update_task.html')
+
+
+if __name__ == '__main__':
+	app.run()
