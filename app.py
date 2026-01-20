83fb3851b80892b2ab715c496cd7b9f666d3bdcc
Michaela D <deftereos_mi@student.kings.edu.au>
2026-01-20 16:35:37 +1100

    Initialise database when user registers

--- app.py
+++ app.py
@@ -6,36 +6,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db' 
 db = SQLAlchemy(app)
 
-# Define a User table schema
-class User(db.Model):
-    name = db.Column('name', db.String(100), primary_key=True)
-    email = db.Column('email', db.String(100), nullable=False, unique=True)
-    password = db.Column('password', db.String(200), nullable=True)
-
-    #constructor method
-    def __init__(self, name, email, password): 
-        self.user_id = name
-        self.email = email
-        self.password = password
-
-    #set and get methods
-    def get_id(self):
-        return self.name
-    
-    def get_email(self):
-        return self.email
-    
-
-# Create the database tables from she
-with app.app_context():
-    db.create_all()
-
-#Helper function to store new user to database
-def admin_auth_register(name_input, email_input, password_input):
-    new_user = User(name=name_input, email=email_input, password=password_input)
-    db.session.add(new_user)
-    db.session.commit()
-
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -44,9 +14,37 @@ def home():
 def login():
     return render_template('login.html')
 
-@app.route('/register', methods=["GET", "POST"])
+# Define a User table schema
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(200), nullable=True)
+
+    #constructor method
+    def __init__(self, email, password): 
+        self.email = email
+        self.password = password
+
+# Create the database tables from she
+with app.app_context():
+    db.create_all()
+
+#Helper function to store new user to database
+def admin_auth_register(email_input, password_input):
+    new_user = User(email=email_input, password=password_input)
+    db.session.add(new_user)
+    db.session.commit()
+
+#POST route to register user and GET to load HTML template
+@app.route('/register/', methods=['POST', 'GET'])
 def register():
-    return render_template('register.html')
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        password_input = request.form.get('password')
+        email_input = request.form.get('email')
+        admin_auth_register(email_input, password_input)
+        return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run()
