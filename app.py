e32181f09aa6bc8c183cc063a94ce3f03bc26115
Michaela D <deftereos_mi@student.kings.edu.au>
2026-02-09 12:08:02 +1100

    input sanitisation

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
+import html
+import regex as re
 from datetime import timedelta
 
 from extensions import db
@@ -44,7 +46,7 @@ def home():
 def register():
     if request.method == 'POST':
         name = request.form.get('name')
-        email = request.form.get('email')
+        email = html.escape(request.form.get('email'))
         password = request.form.get('password')
 
         user = User.query.filter_by(email=email).first()
@@ -53,7 +55,57 @@ def register():
             flash('Email address already exists', 'error')
             return render_template('register.html')
         
-        new_user = User(email=email, name=name, password=generate_password_hash(password))
+        name.lower()
+        cleaned_name = name.replace('\x00', '')
+
+        # Use regex to remove any potential dangerous substrings
+        dangerous_patterns = [r'onload', 
+                            r'onerror', 
+                            r'onclick', 
+                            r'onmouseover', 
+                            r'onfocus', 
+                            r'javascript:', 
+                            r'data:', 
+                            r'<script>', 
+                            r'</script>', 
+                            r'style',]
+        
+        # Replace dangerous substrings with an empty string, case insensitive
+        for pattern in dangerous_patterns:
+            cleaned_name = re.sub(pattern, '', cleaned_name)
+
+        sanitised_name = html.escape(cleaned_name) # in case any HTML tags remain, escape them
+
+        issvalid = 0 # flag to see whether a valid password has been provided
+
+        while issvalid == 0:
+            if len(password) < 8 or len(password) > 64:
+                flash('Password must be between 8 and 64 characters long', 'error')
+                return render_template('register.html')
+        
+            elif re.search(r"[a-z]", password) == None:
+                flash("Password must contain at least one lowercase letter", 'error')
+                return render_template('register.html')
+        
+            elif re.search(r"[A-Z]", password) == None:
+                flash("Password must contain at least one uppercase letter", 'error')
+                return render_template('register.html')
+        
+            elif re.search(r"[0-9]", password) == None:
+                flash("Password must contain at least one digit", 'error')
+                return render_template('register.html')
+        
+            elif re.search(r"[!@#$%^&*]", password) == None:
+                flash("Password must contain at least one special character", 'error')
+                return render_template('register.html')
+
+            else:
+                issvalid = 1
+
+        sanitised_name.split()
+    
+
+        new_user = User(email=email, name=sanitised_name[0], password=generate_password_hash(password))
         db.session.add(new_user)
         db.session.commit()
         flash('Registration successful! Login:', 'success')
