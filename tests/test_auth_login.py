f9f10eca0f41281a71b354a6d59a9aaaa4b97778
Michaela D <deftereos_mi@student.kings.edu.au>
2026-03-30 12:44:55 +1100

    Fix errors and implement login success test

--- tests/test_auth_login.py
+++ tests/test_auth_login.py
@@ -10,14 +10,13 @@ from werkzeug.security import generate_password_hash
 
 class TestUserLogin(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
+        self.client = app.test_client()
+        self.client.testing = True
 
         # Create the database and add a test user
         with app.app_context():
             db.create_all()
-            test_user = User(email='meinna@kings.edu.au')
-            test_user.set_password('correctpassword')  # Hash and set the password
+            test_user = User(email='test@email.com', name='John Smith', password=generate_password_hash('correctpassword'))
             db.session.add(test_user)
             db.session.commit()
 
@@ -25,6 +24,29 @@ class TestUserLogin(unittest.TestCase):
         with app.app_context():
             db.session.remove()
             db.drop_all()
+
+    def test_login_success(self):
+        """
+        Test that login is successful and a session token is given to the user
+        Asserts that:
+            - Response is successful (200)
+            - The user is redirected to the dashboard
+            - If a success json response is given by the server
+            - The session token is given to the user
+        """
+
+        response = self.client.post('/login', data={
+            'email': 'test@email.com',
+            'password': 'correctpassword'
+        })
+        
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertEqual(data['redirect'], '/dashboard')
+        
+        with self.client.session_transaction() as sess:
+            self.assertIn('_user_id', sess) 
 
 if __name__ == '__main__':
     unittest.main()
