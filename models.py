54cc4d93b681f529a1e3c6869b03e6a713be793a
michaela357 <deftereos_mi@student.kings.edu.au>
2026-05-13 19:45:21 +1000

    Improve models.py to implement a method that returns a dictionary of tasks

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 # Define a User table schema
 from extensions import db
 from flask_login import UserMixin
+from datetime import date
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,16 +19,23 @@ class User(UserMixin, db.Model):
         self.quote = quote
 
 class Task(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   due_date = db.Column(db.Date, nullable=False, default=date.today)
-   description = db.Column(db.String(200), nullable=False)
-   complete = db.Column(db.Boolean, default=False)
-   # Define the foreign key
-   user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    due_date = db.Column(db.Date, nullable=False, default=date.today)
+    description = db.Column(db.String(200), nullable=False)
+    complete = db.Column(db.Boolean, default=False)
 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __init__(self, due_date, description, complete, user_id):
         self.due_date = due_date
         self.description = description
         self.complete = complete
         self.user_id = user_id
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'due_date': self.due_date.strftime('%Y-%m-%d'),
+            'description': self.description,
+            'complete': self.complete
+        }      
