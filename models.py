5e5051f7cf833f70f66fba56a08bb5440cb9122f
michaela357 <deftereos_mi@student.kings.edu.au>
2026-05-13 19:35:01 +1000

    Define a task class table in database

--- models.py
+++ models.py
@@ -8,6 +8,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=True)
     quote = db.Column(db.String(250), nullable=True)
+    tasks = db.relationship('Task', backref='user', lazy='dynamic')
 
     #constructor method
     def __init__(self, email, name, password, quote): 
@@ -15,3 +16,18 @@ class User(UserMixin, db.Model):
         self.name = name
         self.password = password
         self.quote = quote
+
+class Task(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   due_date = db.Column(db.Date, nullable=False, default=date.today)
+   description = db.Column(db.String(200), nullable=False)
+   complete = db.Column(db.Boolean, default=False)
+   # Define the foreign key
+   user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+    def __init__(self, due_date, description, complete, user_id):
+        self.due_date = due_date
+        self.description = description
+        self.complete = complete
+        self.user_id = user_id
