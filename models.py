01f44241d470f3245e7dd2743de25e2b83f5a9e0
michaela357 <deftereos_mi@student.kings.edu.au>
2026-05-15 12:44:35 +1000

    Add is_complete to Tasks table

--- models.py
+++ models.py
@@ -24,16 +24,16 @@ class Task(db.Model):
     due_date = db.Column(db.Date, nullable=False, default=date.today)
     description = db.Column(db.String(200), nullable=False)
     reminder = db.Column(db.Boolean, default=False)
-    complete = db.Column(db.Boolean, default=False)
+    is_complete = db.Column(db.Boolean, default=False)
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __init__(self, title, description, due_date, reminder, complete, user_id):
+    def __init__(self, title, description, due_date, reminder, is_complete, user_id):
         self.title = title
         self.description = description
         self.due_date = due_date
         self.reminder = reminder
-        self.complete = complete
+        self.is_complete = is_complete
         self.user_id = user_id
 
     def to_dict(self):
@@ -42,6 +42,12 @@ class Task(db.Model):
             'title': self.title,
             'description': self.description,
             'due_date': self.due_date.strftime('%Y-%m-%d'),
-            'reminder': self.reminder,
-            'complete': self.complete
-        }      
+            'is_complete': self.is_complete,
+            'reminder': self.reminder
+        }
+
+    def set_title(self, title):
+        self.title = title
+    
+    def set_description(self, description):
+        self.description = description
