5563dcadab0a9ca275b3ba3774d4bdc25ef94f90
michaela357 <deftereos_mi@student.kings.edu.au>
2026-06-03 13:05:51 +1000

    Improve commenting for calendar navigation function

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,13 @@
 def get_calendar_navigation(year, month):
-    """Calculates the previous and next month/year pagination values."""
+    """
+    Calculates the previous and next month/year pagination values.
+
+    Args:
+        - year (int): The current year.
+        - month (int): The current month.
+    Returns:
+        - tuple: (prev_year, prev_month, next_year, next_month)
+    """
     if month > 1:
         prev_month, prev_year = month - 1, year
     else:
